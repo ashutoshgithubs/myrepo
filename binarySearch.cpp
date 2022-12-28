2947ca1c18ac25604c72567bfe6c213eddeaf7f3
ashutoshgithubs <kumarashutosh1956@gmail.com>
2022-12-28 22:41:35 +0530

    binarySearch.cpp updated
    There was an error. I have removed it and code is working good.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -29,12 +29,9 @@ int binary_search(int Arr[], int n, int key)
 int main()
 {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 11, 15, 90}; // Array must be sorted either ascending or decending
-    binary_search(arr, 10, 7);
-    binary_search(arr, 10, 15)
-
-            cout
-        << "The index of key is:" << binary_search(arr, 10, 7) << endl;
-    cout << "The index of key is:" << binary_search(arr, 10, 15) << endl;
+    binary_search(arr, 8, 7);
+    cout << "The index of key is:" << binary_search(arr, 10, 7);
+    cout << "The index of key is:" << binary_search(arr, 10, 15);
 
     return 0;
 }
